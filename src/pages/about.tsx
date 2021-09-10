44682045b397c4e1b2023a244a9b8f9ae72a809f
Stackbit Code Editor <projects@stackbit.com>
2021-09-10 22:59:07 +0000

    about.tsx: changed

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -5,7 +5,7 @@ const About = () => (
   <Main meta={<Meta title="Lorem ipsum" description="Lorem ipsum" />}>
     <p>
       TERECIBO es una aplicación web disponible para dispositivos móviles, tabletas y computadoras de escritorio que permite a los compradores en línea conectarse con un vecino (neighbor) o negocio local validado y de confianza que firmará y retendrá las entregas para ellos.
-      <br
+      <br></br>
       Todos los vecinos son mayores de 21 años y por $ por envÍo, puede reservar un NEIGHBOR para casi cualquier cosa. Después de que su NEIGHBOR reciba su entrega, usted se encarga de recogerla a la hora que le resulte conveniente.
 
       Al pagar en cualquier sitio, descubra sus fechas de envío estimadas y abra una nueva pestaña o use su teléfono inteligente o tableta y vaya a terecibo.com. Ingrese la (s) fecha (s) proporcionada al finalizar la compra para buscar y reservar un NEIGHBOR en segundos.
