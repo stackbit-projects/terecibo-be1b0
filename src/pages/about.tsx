5735cb1f38ba46176d3d88884a6d9d01bbb25b42
Stackbit Code Editor <projects@stackbit.com>
2021-09-10 23:00:10 +0000

    about.tsx: changed

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -5,7 +5,6 @@ const About = () => (
   <Main meta={<Meta title="Lorem ipsum" description="Lorem ipsum" />}>
     <p>
       TERECIBO es una aplicación web disponible para dispositivos móviles, tabletas y computadoras de escritorio que permite a los compradores en línea conectarse con un vecino (neighbor) o negocio local validado y de confianza que firmará y retendrá las entregas para ellos.
-
     </p>
     <p>
       Al pagar en cualquier sitio, descubra sus fechas de envío estimadas y abra una nueva pestaña o use su teléfono inteligente o tableta y vaya a terecibo.com. Ingrese la (s) fecha (s) proporcionada al finalizar la compra para buscar y reservar un NEIGHBOR en segundos.
