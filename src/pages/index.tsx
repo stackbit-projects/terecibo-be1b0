521508ad86d6541a435fa65b5e1e7bc3455d7e53
Stackbit Code Editor <projects@stackbit.com>
2021-09-10 23:56:22 +0000

    index.tsx: changed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -39,7 +39,7 @@ const Index = () => {
         Disminuimos la tasa de rebote de las empresas de envíos y nos aseguramos que el paquete llegue en la fecha indicada.
       </p>
       <h2 className="font-semibold text-lg">Next js Boilerplate Features</h2>
-      <p>¿Por qué debes utilizar TeRECIBO?</p>
+      <p></p>
       <ul>
         <li>
           <span role="img" aria-label="fire">
